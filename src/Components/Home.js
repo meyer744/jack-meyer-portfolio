98eac6111d5597e6ee8dd00329ce3b1a061395a9
Meyer <jack.meyer@emerson.com>
2020-06-13 00:15:57 -0400

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ProfilePic from '../Assets/jack-meyer-pic.jpg'
+
+const Home = () => {
+    return (
+        <div className="home">        
+            <img className="profile-pic" src={ProfilePic} alt="Jack-Meyer"/>
+            <h2 className="profile-pic-heading">Jack Meyer</h2>
+            <ul className="profile-blurb">
+                <li className="profile-blurbs">Web Developer</li>
+                <li className="profile-blurbs">Experience in Digital Marketing & IT</li>
+                <li className="profile-blurbs">Proud father of two awesome kids</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Home;
