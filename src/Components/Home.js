94a7ad5244619b94a3ab9e3df78b7ea789008e6d
Meyer <jack.meyer@emerson.com>
2020-06-24 14:13:46 -0400

    added flex style to home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,14 +3,18 @@ import ProfilePic from '../Assets/jack-meyer-pic.jpg'
 
 const Home = () => {
     return (
-        <div id="Home" className="home">        
+        <div id="Home" className="home">
+        <div className="home-container">    
             <img className="profile-pic" src={ProfilePic} alt="Jack-Meyer"/>
+            <div className="home-right">
             <h2 className="profile-pic-heading">Jack Meyer</h2>
             <ul className="profile-blurb">
                 <li className="profile-blurbs">Web Developer</li>
                 <li className="profile-blurbs">Experience in Digital Marketing & IT</li>
                 <li className="profile-blurbs">Proud father of two awesome kids</li>
             </ul>
+            </div>
+            </div>    
         </div>
     );
 };
